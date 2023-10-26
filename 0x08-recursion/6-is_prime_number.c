825e20b5373714400d07bd693f3f58ae9797b98d
Kirui-brian <brinkiru@gmail.com>
2023-10-26 02:11:18 -0700

    add 6-is_prime_number.c file

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * is_prime_number - A function to tell prime & non prime numbers.
+ * @n: The number to evaluate.
+ *
+ * Return: 1 if prime 0 otherwise.
+ */
+
+int is_prime_number(int n)
+{
+	if  (n <= 1)
+		return (0);
+	return (prime_number(n, n - 1));
+}
+
+/**
+ * prime_nummber - Evaluated recursively is a number is prime.
+ * @n: The number to evaluate.
+ * @i: The iterator.
+ *
+ * Return: 1 if prime 0 otherwise.
+ */
+
+int prime_number(int n, int i)
+{
+	if (i ==1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (prime_number (n, i - 1));
+}
