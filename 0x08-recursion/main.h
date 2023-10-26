e3c644663194072fceb2c02fa43632f3970bffbc
Kirui-brian <brinkiru@gmail.com>
2023-10-26 02:37:56 -0700

    update main.h file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,7 +3,7 @@
 
 void _puts_recursion(char *s);
 int _putchar(char c);
-void _print_rev_recursion(char *S);
+void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
@@ -11,5 +11,6 @@ int _sqrt_recursion(int n);
 int nat_sqrt_recursion(int n, int i);
 int is_prime_number(int n);
 int prime_number(int n, int i);
-int is_palindrome (char *s);
+int is_palindrome(char *s);
+int check_palindrome(char *s, int i, int length);
 #endif
