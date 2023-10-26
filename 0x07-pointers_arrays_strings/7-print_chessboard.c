e9730c398b3cd9020977b0cfad317fd3fd568c30
Kirui-brian <brinkiru@gmail.com>
2023-10-26 07:37:27 -0700

    add print chess board file

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints the chessboard.
+ * @a: Input pointer.
+ *
+ * Return: Always 0.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (i < 64)
+	{
+		if (i % 8 == 0 && i !=0)
+		{
+			j = i;
+			_putchar('\n');
+		}
+
+		_putchar(a[i / 8][i - j]);
+		i++;
+	}
+	_putchar('\n');
+}
