7b4243a6a2d317128e7e66c5fa1b090b1346f66d
Kirui-brian <brinkiru@gmail.com>
2023-11-07 23:52:00 -0800

    initial commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#define NAME _file_
+
+#endif
+
+/**
+ * main - prints the name of the file it was compiled from,
+ * 	followed by a new line.
+ *
+ * Return: Success(0).
+ */
+int main(void)
+{
+	printf("%s\n", NAME);
+
+	return (0);
+}
