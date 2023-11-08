7e3f589b01c110cb9f42a77ed4d3175e8ffe7f19
Kirui-brian <brinkiru@gmail.com>
2023-11-08 01:09:31 -0800

    add corrected 4-add.c file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,21 +12,20 @@
  * Return: If one of the numbers contains symbols that are non-digits - 1.
  *         Otherwise - 0.
  */
-
 int main(int argc, char **argv)
 {
-    int sum, i;
-    int b;
-
-    sum = 0;
-
-    /* Check if  no arg was passed */
-    for (i = 1; i < argc; i++)
-    {
+	int sum, i;
+	int b;
+	
+	sum = 0;
+	
+	/* Check if  no arg was passed */
+	for (i = 1; i < argc; i++)
+	{
         /* Check if any char passed to arg is not a No. */
 	for (b = 0; argv[i][b]; b++)
 	{
-		if (isdigit[i][b]) == 0)
+		if ((isdigit[i][b]) == 0)
 		{
 			printf("Error\n");
 			return (1);
