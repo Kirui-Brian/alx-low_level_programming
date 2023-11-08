ef267bd3d0c39a05914e035dc44f16d3ea774327
Kirui-brian <brinkiru@gmail.com>
2023-11-08 05:30:40 -0800

    add corrected 3-main.c file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,28 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - Entry point.
- * @argc: No. of args.
- * @argv: Arguments.
- *
- * Return: Success/Error code.
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array of command line arguments
+ * Return: 0 on success, other values on failure
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *op;
-
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
-
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
-
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	if (get_op_func(op)(num1, num2) == '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	printf("%d\n", get_op_func(op)(num1, num2));
-
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
+	char *operator = argv[2];
+	
+	int (*func)(int, int) = get_op_func(operator);
+	int result = func(num1, num2);
+	
+	printf("%d\n", result);
 	return (0);
 }
