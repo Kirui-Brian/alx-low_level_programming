5e9fca131bd0db8367f9e8acf6e623eebc1c681a
Kirui-brian <brinkiru@gmail.com>
2023-11-08 00:44:45 -0800

    add corrected 2-args.c file

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
     /* Iterate through the arguments and print each one */
     while (argc--)
     {
-	    printf('%s\n', *argv++);
+	    printf("%s\n", *argv++);
     }
     
     return (0);
