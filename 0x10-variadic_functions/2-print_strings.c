8e59002f62472f9a50a03c043a4dfb28aee41005
Kirui-brian <brinkiru@gmail.com>
2023-11-09 01:05:42 -0800

    add variadic_functions.h file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
-#include "variadic_functions"
+#include "variadic_functions.h"
 
 /**
  * print_strings - Prints strings, separated by a specified separator.
