1bf5d27e8076dacf2ae452cb6dd6c7897945090d
Kirui-brian <brinkiru@gmail.com>
2023-11-09 02:13:51 -0800

    correction

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,18 +11,28 @@
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2, result;
+	char *operator;
+	int (*func)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		return (98);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
-	char *operator = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
 	
-	int (*func)(int, int) = get_op_func(operator);
-	int result = func(num1, num2);
+	func = get_op_func(operator);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	result = func(num1, num2);
 	
 	printf("%d\n", result);
 	return (0);
