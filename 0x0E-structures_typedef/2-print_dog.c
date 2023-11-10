817376dddfc9e33c87c74ab04ef178effb71b9bf
Kirui-brian <brinkiru@gmail.com>
2023-11-09 23:46:43 -0800

    initial commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - Prints a struct dog.
+ * @d: Pointer to the struct dog to print.
+ *
+ * Description: This function prints the attributes of a struct dog. If any
+ * 	attribute is NULL, it prints "(nil)" for that atrribute.
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+	}
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees a dog_t.
+ * @d: Pointer to the dog_t to free.
+ *
+ * Description: This function frees the memory allocatedfor a dog_t,
+ * 	including the copied name and owner strings.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
