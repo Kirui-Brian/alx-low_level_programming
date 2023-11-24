8253804ba895277f39e871306989d01647885731
Kirui-brian <brinkiru@gmail.com>
2023-11-24 08:12:43 -0800

    correct 100-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,18 @@
 section .data
-    hello db 'Hello, Holberton', 0
+	hello db 'Hello, Holberton', 0
 
 section .text
-    global main
-    extern printf
+	global main
+	extern printf
 
 main:
-    push rbp
-    mov rdi, hello
-    call printf
-    pop rbp
+	push rbp
+	mov rdi, hello
+	call printf
+	mov rax, 0	; printf is a varargs function, so we need to clear the vector registe
+	pop rbp
 
-    ; Exit the program
-    mov rax, 60         ; syscall: exit
-    xor rdi, rdi        ; status: 0
-    syscall
+	; Exit the program
+	mov rax, 60         ; syscall: exit
+	xor rdi, rdi        ; status: 0
+	syscall
