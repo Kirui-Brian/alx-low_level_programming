505d3f7b18bdca5d500d5e00f040c78ce7ff37fc
Kirui-brian <brinkiru@gmail.com>
2023-11-24 07:03:23 -0800

    correct 100-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,17 +2,22 @@ section .data
 	hello db "Hello, Holberton", 0
 
 section .text
-	global print_hello
+	global _start
 
-print_hello:
-	; Call write system call (syscall number 1)
-	mov rax, 1	; syscall number for sys_write
-	mov rdi, 1	; file descriptor 1 is stdout
-	mov rsi, hello	; pointer to the string
-	mov rdx, 18	; length of the string
-	syscall
+	_start:
+		; Call write system call (syscall number 1)
+		mov rdi, hello	; format string
+		mov rax, 0	; syscall number for sys_write
+		call printf
 
-	ret
+		; Exit the program
+		mov rax, 60	; syscall number for sys_exit
+		xor rdi, rdi	; exit code 0
+		syscall
 
-section .data
-	newline db 10	; newline character
+section .text
+	; External function declarations
+	extern printf
+
+section .bss
+	; BSS section for uninitialized data
