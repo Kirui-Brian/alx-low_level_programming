9b08b3c72f6aff4eb1e4302c218628da2b40b381
Kirui-brian <brinkiru@gmail.com>
2023-11-24 05:54:32 -0800

    correct 100-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,21 +3,14 @@ section .data
 
 section .text
 	global _start
+	extern printf
 
-_start
+_start:
 	; Call printf
 	mov rdi, hello	; format string
-	mov rax, 0	; syscall number for sys_write
 	call printf
 
 	; Exit the program
 	mov rax, 60	; syscall number for sys_exit
 	xor rdi, rdi	; exit code 0
 	syscall
-
-section .text
-	; External function declarations
-	extern printf
-
-section .bss
-	; BSS section for uninitialized data
