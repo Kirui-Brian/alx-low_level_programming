b3d181e96369c3021f599ddb926c338a7d403581
Kirui Brian <brinkiru@gmail.com>
2024-02-24 12:22:21 +0300

    add 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,43 +1,44 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position
- * @h: Pointer to the address of the head of the list
- * @idx: Index of the list where the new node should be added
- * @n: Integer value to be stored in the new node
+ * insert_dnodeint_at_index - Inserts a new node at a given position.
+ * @h: Pointer to the head of the list.
+ * @idx: Index of the list where the new node should be added.
+ * @n: Value to store in the new node.
  *
- * Return: Address of the new node, or NULL if it failed
+ * Return: Address of the new node, or NULL if it failed.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *current = *h;
-    unsigned int i = 0;
+	dlistint_t *new_node, *current;
+	unsigned int i = 0;
 
-    if (idx == 0)
-        return (add_dnodeint(h, n));
+	if (h == NULL)
+		return (NULL);
 
-    while (current != NULL && i < idx - 1)
-    {
-        current = current->next;
-        i++;
-    }
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-    if (current == NULL)
-        return (NULL);
+	current = *h;
+	while (current != NULL && i < idx - 1)
+	{
+		current = current->next;
+		i++;
+	}
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+	if (current == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->prev = current;
-    new_node->next = current->next;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    if (current->next != NULL)
-        current->next->prev = new_node;
+	new_node->n = n;
+	new_node->next = current->next;
+	new_node->prev = current;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
 
-    current->next = new_node;
-
-    return (new_node);
+	return (new_node);
 }
-
