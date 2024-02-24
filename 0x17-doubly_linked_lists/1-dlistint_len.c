024375b03f9add9bb8e65a204ab4d6488f546561
Kirui Brian <brinkiru@gmail.com>
2024-02-24 11:24:54 +0300

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,13 +8,13 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while (h != NULL)
-    {
-        len++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
 
-    return (len);
+	return (len);
 }
