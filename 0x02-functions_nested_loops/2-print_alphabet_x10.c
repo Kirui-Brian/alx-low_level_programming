dec087753f16b37dda3ce849bd6b70ee3567a9d2
Kirui Brian <brinkiru@gmail.com>
2024-03-26 17:27:06 +0300

    add correct 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10.c - Check description
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  * Description: function uses _putchar to print alphabet 10 times
  * Return: Nothing
  */
