49817fa3d29389c680b1f4fd937735f5bdc42e4f
Kirui Brian <brinkiru@gmail.com>
2024-03-26 17:31:18 +0300

    add correct 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,14 +7,8 @@
  */
 int _islower(int c)
 {
-	char i;
-	int lower = 0;
-
-	for (i = 'a'; i = 'z'; i++)
-	{
-		if (i == c)
-			lower = 1;
-	}
-
-	return(lower);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
