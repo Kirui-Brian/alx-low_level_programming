df32e3e45349165f2a38ad2cb7dd09a86677a938
Kirui Brian <brinkiru@gmail.com>
2024-03-27 09:10:53 +0300

    add correct 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * void jack_bauer - Check description
- * Description:  a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - A function that prints every minute of the day of Jack Bauer, 
+ *	starting from 00:00 to 23:59
+ *
  * Return: Nothing
  */
 void jack_bauer(void)
