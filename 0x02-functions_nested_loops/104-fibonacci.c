b54186138580b9b94d96c54f67eacfde28fa3817
Kirui Brian <brinkiru@gmail.com>
2024-03-27 10:34:10 +0300

    add correct 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,12 +2,10 @@
 #define LARGEST 10000000000
 
 /**
- * a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.
- * The numbers should be separated by comma, followed by a space ,
- * You are allowed to use the standard library
- * You are not allowed to use any other library (You can’t use GMP etc...)
- * You are not allowed to use long long, malloc, pointers, arrays/tables, or structures
- * You are not allowed to hard code any Fibonacci number (except for 1 and 2)
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ *        1 and 2, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
