c4bff7144050697d82e2dfc9ac9643245ac28e14
Kirui Brian <brinkiru@gmail.com>
2024-03-27 10:14:34 +0300

    add correct 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,55 +1,46 @@
 #include "main.h"
 
 /**
- * void print_times_table - Check desc.
- * Desc: a function that prints the n times table, starting with 0.
- * If n is greater than 15 or less than 0 the function should not print anything.
+ * print_times_table - A function that prints the n times table, starting with 0.
+ * If n > 15 or < 0 the function should not print anything.
+ * @n: The value of the times table to be printed.
+ *
  * Return nothing.
  */
 void print_times_table(int n)
 {
-	int i, j;
+	int num, mul, prod;
 
-	if (n > 0 && n < 15)
+	if (n >= 0; n <= 15)
 	{
-		for (i = 0; i <= n; i++)
+		for (num = 0; num <= n; num++)
 		{
-			_putchar('0);
-			for (j = 0; j <= n; j++)
-				putformat(i * j);
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
 			_putchar('\n');
 		}
-	}
-}
-
-/**
- * putformat - formatted characters to output
- * @n - number to format
- * Return nothing
- */
-void putformat(int n)
-{
-	if (n <=9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
 	}
 }
